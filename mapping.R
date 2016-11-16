b36933d8cc2e4d7ae2d2b6c739b5173acd1c957a
jcoliver <jeffrey.oliver@science.oregonstate.edu>
2016-11-16 11:39:17 -0700

    Initial commit of project

---
+++ mapping.R
@@ -0,0 +1,11 @@
+# Graphing data for viceroy paper
+# Jeffrey C. Oliver
+# jcoliver@email.arizona.edu
+# 2016-11-16
+
+# See:
+# https://mgimond.github.io/Spatial/interpolation-in-r.html
+# https://en.wikipedia.org/wiki/Inverse_distance_weighting
+# http://www.geo.ut.ee/aasa/LOOM02331/R_idw_interpolation.html
+# http://people.oregonstate.edu/~knausb/R_group/maps_idw.r
+# https://cran.r-project.org/web/packages/gstat/gstat.pdf
