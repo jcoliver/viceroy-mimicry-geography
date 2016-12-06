541178de731c50b03c6e8c44f3dda2be177239bb
jcoliver <jcoliver@email.arizona.edu>
2016-12-05 17:17:12 -0700

    Placeholders for chemical defense and palatability scripts

---
+++ mapping-palatability.R
@@ -0,0 +1,11 @@
+# INSERT CODE DESCRIPTION
+# Jeff Oliver
+# jcoliver@email.arizona.edu
+# 2016-12-05
+
+rm(list = ls())
+
+################################################################################
+# SUMMARY
+# Use latitude & longitude coordinates to create maps of palatability in 
+# viceroys (aversion learning & memory retention)
