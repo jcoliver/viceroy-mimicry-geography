a9bb9c6711acab2598c2090681d70557a9ea3566
jcoliver <jeffrey.oliver@science.oregonstate.edu>
2017-02-24 20:19:43 -0700

    First draft of boxplots

--- barcharts-development.R
+++ barcharts-development.R
@@ -4,6 +4,7 @@
 # 2017-02-22
 
 rm(list = ls())
+source(file = "barcharts-globals.R")
 
 ################################################################################
 # SUMMARY
@@ -33,3 +34,26 @@ plot.dims <- c(2, 2) # two rows, two columns
 # Prepare data
 
 plot.data <- read.delim(file = data.file)
+
+north.values <- plot.data[plot.data$Site.Name %in% barchart.globals$north.pops, plots$variables[1]]
+south.values <- plot.data[plot.data$Site.Name %in% barchart.globals$south.pops, plots$variables[1]]
+
+boxplot.data <- data.frame(group = c(rep(x = "north", times = length(north.values)), rep(x = "south", times = length(south.values))),
+                           values = c(north.values, south.values))
+
+boxplot(barplot.data$values ~ barplot.data$group,
+        border = c("#444444", "black"),
+        col = c("black", "#444444"))
+
+library("ggplot2")
+pdf(file = "~/Desktop/total-phenolics-viceroys.pdf", useDingbats = FALSE)
+box.plot <- ggplot(data = barplot.data, 
+                   aes(x = group, y = values, fill = group)) +
+  geom_boxplot() + 
+  scale_fill_manual(values = c("white", "black")) +
+  labs(y = plots$plot.titles[1]) + 
+  theme(axis.title.x = element_blank(), # no title on x
+        axis.ticks.x = element_blank(), # no label on x
+        legend.position = "none") # no legend
+box.plot
+dev.off()
