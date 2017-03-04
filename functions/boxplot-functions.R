8a5c8cc118ebab5831ebf998f59893e135dd2eca
jcoliver <jcoliver@email.arizona.edu>
2017-03-03 17:40:57 -0700

    Add file that will ultimately have boxplot functions

---
+++ functions/boxplot-functions.R
@@ -0,0 +1,18 @@
+# Functions for boxplots
+# Jeff Oliver
+# jcoliver@email.arizona.edu
+# 2017-03-03
+
+################################################################################
+MakeBoxplot <- function(plot.data, group.name, value.name) {
+  
+  
+  
+  MakeFloridaMap(plot.data = plot.data,
+                 variable.name = variable.name,
+                 variable.text = variable.text,
+                 map.shade.colors = plotting.globals$map.colors,
+                 map.point.colors = map.point.colors,
+                 map.point.bg = point.fill)
+  
+}
