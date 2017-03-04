ac7717390753da78dfad0513f6fc5c49102fe3c5
jcoliver <jcoliver@email.arizona.edu>
2017-03-03 17:41:30 -0700

    Some reorganization and abstraction in map-and-boxplot

--- map-and-boxplot.R
+++ map-and-boxplot.R
@@ -4,13 +4,14 @@
 # 2017-02-28
 
 rm(list = ls())
-source(file = "functions/mapping-functions.R")
-source(file = "plotting-globals.R")
 
 ################################################################################
 # SUMMARY
 # Creates two plots, a map on the left and a boxplot on the right
 
+################################################################################
+# SPECIFICS
+# Add information unique to this set of figures
 data.file <- "data/chemistry-viceroy-data.txt"
 output.file <- "output/Volatiles-two-panel"
 vars <- data.frame(var.name <- c("Total.Volatiles",
@@ -21,14 +22,25 @@ vars <- data.frame(var.name <- c("Total.Volatiles",
                                  "Benzaldehyde (units)"),
                    stringsAsFactors = FALSE)
 
+################################################################################
+# SETUP
+# Load source files
+# Read in data
+# Prepare data
+
+source(file = "functions/mapping-functions.R")
+source(file = "functions/boxplot-functions.R")
+source(file = "plotting-globals.R")
+
 # Read in data
 # Read data with latitude, longitude, and whatever variable(s) to graph
 plot.data <- read.delim(file = data.file)
 
 # Set color values for graphics
-map.point.colors <- "black"
-point.fill <- rep(x = "black", times = nrow(x = plot.data))
-point.fill[plot.data$Site.Name %in% plotting.globals$north.pops] <- "white"
+map.point.outline <- "black"
+group.cols <- c("black", "white")
+group.fill <- rep(x = group.cols[1], times = nrow(x = plot.data))
+group.fill[plot.data$Site.Name %in% plotting.globals$north.pops] <- group.cols[2]
 
 # Prep output file name
 file.format <- plotting.globals$output.format
@@ -54,30 +66,30 @@ for (variable in 1:nrow(vars)) {
                  variable.name = variable.name,
                  variable.text = variable.text,
                  map.shade.colors = plotting.globals$map.colors,
-                 map.point.colors = map.point.colors,
-                 map.point.bg = point.fill)
+                 map.point.outline = map.point.outline,
+                 map.point.bg = group.fill)
   
-  north.values <- plot.data[plot.data$Site.Name %in% plotting.globals$north.pops, variable.name]
-  south.values <- plot.data[plot.data$Site.Name %in% plotting.globals$south.pops, variable.name]
+  north.value <- plot.data[plot.data$Site.Name %in% plotting.globals$north.pops, variable.name]
+  south.value <- plot.data[plot.data$Site.Name %in% plotting.globals$south.pops, variable.name]
   
   # Boxplot
   par(mar = c(1.5, 3, 1, 1))
-  boxplot.data <- data.frame(group = c(rep(x = "North", times = length(north.values)), rep(x = "South", times = length(south.values))),
-                             values = c(north.values, south.values))
+  boxplot.data <- data.frame(group = c(rep(x = "North", times = length(north.value)), rep(x = "South", times = length(south.value))),
+                             value = c(north.value, south.value))
   
-  boxplot(boxplot.data$values ~ boxplot.data$group, 
+  boxplot(boxplot.data$value ~ boxplot.data$group, 
           frame = FALSE,
           axes = FALSE)
   rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = 
          "#DDDDDD", border = NA)
-  boxplot(boxplot.data$values ~ boxplot.data$group, 
+  boxplot(boxplot.data$value ~ boxplot.data$group, 
           add = TRUE,
           ylab = "",
           yaxt = "n",
           xaxt = "n",
           bg = c("#666666"),
-          medcol = c("black", "white"),
-          col = c("white", "black"), 
+          medcol = c(group.cols[1], group.cols[2]),
+          col = c(group.cols[2], group.cols[1]), 
           frame = FALSE,
           las = 1)
   axis(side = 1, at = c(1, 2), labels = c("North", "South"), las = 1, lwd = 0, line = -0.5)
