deae63dc9846d41c0b6565fb4db9a8abd1a3bfdb
jcoliver <jcoliver@email.arizona.edu>
2017-06-23 17:21:57 -0700

    Add twinevine abundance predicts viceroy chemistry

---
+++ analysis-twinevine-on-viceroy-phenolics.R
@@ -0,0 +1,60 @@
+# Analyzing twinevine abundance on viceroy chemical defense (volatile & non-volatile)
+# Jeffrey C. Oliver
+# jcoliver@email.arizona.edu
+# 2017-06-16
+
+rm(list = ls())
+
+################################################################################
+# SETUP
+# Load dependencies
+library("lmerTest")
+
+# Read in data
+chem.data <- read.delim(file = "data/chemistry-viceroy-data.txt")
+abundance.data <- read.delim(file = "data/abundance-data.txt")
+
+# Set destination for results
+output.file <- "output/analysis-results/twinevine-on-viceroy-phenolics.txt"
+
+# Enumerate the response variables to analyze
+responses <- c("Total.Phenolics",
+               "Salicin",
+               "Salicortin",
+               "Tremulacin",
+               "Total.Volatiles",
+               "Salicylaldehyde",
+               "Benzaldehyde")
+
+# ----------------      SHOULD NOT NEED TO EDIT BELOW HERE      ----------------
+
+# Replace column name in abundance data so we can easily merge the two data frames
+colnames(abundance.data)[which(colnames(abundance.data) == "Observation.Date")] <- "Collection.Date"
+all.data <- merge(x = chem.data, 
+                  y = abundance.data)
+
+################################################################################
+# ANALYSES
+# Run linear model, with Site.Name and Collection.Date as random effects
+
+# Data frame to hold results
+model.results <- data.frame(response = responses,
+                            coeff = NA,
+                            df = NA,
+                            t = NA,
+                            p = NA)
+
+# Iterate over all response variables and run model
+for (response.index in 1:length(responses)) {
+  response <- responses[response.index]
+  chem.model <- lmer(eval(as.name(response)) ~ Number.Twinevine.Plants + (1|Site.Name) + (1|Collection.Date),
+                     data = all.data)
+  chem.summary <- summary(chem.model)
+  model.results[response.index, c(2:ncol(model.results))] <- chem.summary$coefficients[2, c(1, 3:5)]
+}
+
+write.table(x = model.results, 
+            file = output.file, 
+            row.names = FALSE, 
+            quote = FALSE,
+            sep = "\t")
